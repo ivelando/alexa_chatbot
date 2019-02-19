eeb26f7f4e6bb3d2daa4c4535e6c9a0f9868f587
gaston-arg <gaston.unlock@gmail.com>
2019-02-19 11:36:35 -0300

    Update service.js

--- service.js
+++ service.js
@@ -21,8 +21,8 @@ module.exports = new function() {
     allowConfigUpdate: true, //set to false to turn off REST endpoint of allowing update of metadata
     waitForMoreResponsesMs: 200,  //milliseconds to wait for additional webhook responses
     amzn_appId: "amzn1.ask.skill.c6437362-97fd-40ff-80d6-8fce05036b6d",
-    channelSecretKey: 'dPXrwaDlVjjeXwxcAfm764KNEAqDy8x8',
-    channelUrl: 'https://botv2frk1I0065H26E43Ebots-mpaasocimt.botmxp.ocp.oraclecloud.com:443/connectors/v1/tenants/idcs-6d466372210e4300bb31f4db15e8e96c/listeners/webhook/channels/592e9e31-ab36-4f87-bb16-51ad9d53576b'
+    channelSecretKey: 'm9XleQmiIfsl8TLIlw310a6iDcomF7A8',
+    channelUrl: 'https://botv2iad1I0100H203896bots-mpaasocimt.botmxp.ocp.oraclecloud.com:443/connectors/v1/tenants/idcs-100b89d671b54afca3069fe360e4bad4/listeners/webhook/channels/2afbdde0-5aa4-44bc-afcf-41768b8d7eb1'
   };
 
   this.randomIntInc = function (low, high) {
